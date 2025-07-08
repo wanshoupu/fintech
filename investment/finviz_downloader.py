7cd1f42d64804c1b7fe470126f3f17629bb16a39
Shoupu Wan <wanshoupu@gmail.com>
2025-07-07 17:34:56 -0700

    Update the user interface with argparser -v view type -s start offset -l length limit

--- investment/finviz_downloader.py
+++ investment/finviz_downloader.py
@@ -1,11 +1,17 @@
 import asyncio
+import argparse
 
 from investment.utils.finviz_scraper import scrape_to_file
 from investment.utils.finviz_views import FinVizView
 
 if __name__ == '__main__':
-    size = 10
-    view_type = FinVizView.ALL
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--view", help="The source to parse, if any", default="ALL")
+    parser.add_argument("-s", "--start", help="The offset to start at, if any", default=10, type=int)
+    parser.add_argument("-l", "--length", help="The length limit of the scaper, if any", type=int)
+    args = parser.parse_args()
 
-    start = 1
-    asyncio.run(scrape_to_file(view_type, start_offset=start))
+    view_type = FinVizView[args.view]
+    start = args.offset
+    length = args.length
+    asyncio.run(scrape_to_file(view_type, start_offset=start, length=length))
