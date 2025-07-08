22327cdeb42ed87d188b26bc9850d5950c41c329
Shoupu Wan <wanshoupu@gmail.com>
2025-07-07 17:44:14 -0700

    minor change start at 1 by default

--- investment/finviz_downloader.py
+++ investment/finviz_downloader.py
@@ -7,7 +7,7 @@ from investment.utils.finviz_views import FinVizView
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--view", help="The source to parse, if any", default="ALL")
-    parser.add_argument("-s", "--start", help="The offset to start at, if any", default=10, type=int)
+    parser.add_argument("-s", "--start", help="The offset to start at, if any", default=1, type=int)
     parser.add_argument("-l", "--length", help="The length limit of the scaper, if any", type=int)
     args = parser.parse_args()
 
