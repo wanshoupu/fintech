3cf1fbd5d1e8c1ee75d5aa0956abb58c41d7f8be
Shoupu Wan <wanshoupu@gmail.com>
2025-07-09 14:06:38 -0700

    fix unit tests

--- tests/utils/test_finviz_formatter.py
+++ tests/utils/test_finviz_formatter.py
@@ -10,11 +10,12 @@ def test_finviz_formatter():
     parent_dir = Path(__file__).resolve().parent.parent
     data_file = os.path.abspath(os.path.join(parent_dir, 'data', 'finviz-ALL-1-10220.parquet'))
     df = pd.read_parquet(data_file)
-    formatted_df = clean_finviz(df)
+    df = clean_finviz(df)
 
-    print(formatted_df.columns.tolist())
-    types = formatted_df.dtypes.describe()
-    assert types['count'] == formatted_df.shape[1]
-    assert types['unique'] == 12
+    # print(df.columns.tolist())
+    types = df.dtypes.describe()
+    # print(types)
+    assert types['count'] == df.shape[1]
+    assert types['unique'] == 11
     assert types['top'] == 'float64'
     assert types['freq'] == 104
